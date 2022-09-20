8f161a26ea348df9be5ab00b66164d113ca65256
Andrey G <a.gavrilov@promo-bot.ru>
2022-09-20 15:56:00 +0500

    Update app.js

--- 00-js-basics/app.js
+++ 00-js-basics/app.js
@@ -175,3 +175,117 @@ NaN
 
 // Добавьте код здесь, переписав конструкцию выше на тернарный оператор
 // a + b < 4 ? (result = 'Мало') : (result = 'Много')
+
+// Массивы
+
+// const arr = [12, 13, 15, 14, 55]
+// console.log(arr[0])
+// console.log(arr[arr.length - 1])
+
+// const strs = ['abc', 'cde']
+
+// strs.push('new item')
+// strs.push('new item 2')
+
+// console.log(strs)
+// strs.pop()
+// console.log(strs)
+
+// // Задание 3
+// // 3.1) Создайте массив numbers и сохраните в нем 3 числа - 1, 2 и 3
+// const numbers = [1, 2, 3]
+
+// // 3.2) Создайте переменную luckyNumber и сохраните в него последнее число из массива (используя индекс массива).
+// const luckyNumber = numbers[2]
+
+// // 3.3) Удалите последний элемент массива и добавьте в конец массива два новых числа.
+// numbers.pop()
+// numbers.push(4, 5)
+// // numbers.push(5)
+
+// console.log(numbers)
+// console.log(luckyNumber)
+
+// Циклы
+// const numbers = [1, 2, 3, 55, 52, 66, 74, 35, 89]
+
+// let index = 0
+
+// while (index < numbers.length) {
+//   numbers[index] *= 2
+//   console.log(numbers[index])
+//   index++
+// }
+
+// work -
+
+// Дан массив чисел. В цикле while обойдите массив и значения всех элементов с нечетным индексом
+// умножьте на 2.
+// Значения элементов с четным индексом должны остаться без изменения.
+
+// const numbers = [2, 7, 19, 22, 38, 9, 11, 10, 10]
+// let index = 0
+// while (index < numbers.length) {
+//   if (index % 2 !== 0) {
+//     numbers[index] *= 2
+//     console.log(numbers[index])
+//   }
+//   index++
+// }
+
+// FOR
+
+// const numbers = [2, 7, 19, 22, 38, 9, 11, 10, 100]
+
+// for (let i = 0; i < numbers.length; i++) {
+//   numbers[i] *= 2
+//   console.log(numbers[i])
+// }
+
+// const numbers = [1, 2, 3, 4, 5, 6]
+// for (let i = 0; i < numbers.length; i++) {
+//   if (i % 2 === 0) {
+//     numbers[i] += 3
+//   }
+// }
+
+// ЦИКЛ break
+
+// let tries = 0
+
+// while (tries < 3) {
+//   let age = +prompt('Сколько вам лет?')
+
+//   if (age) {
+//     alert('Welcome')
+//     break
+//   }
+//   tries++
+//   alert('Введите число')
+// }
+// alert('end')
+
+// Оператор continue
+
+// work
+
+//Обойдите циклом массив чисел numbers и сохраните в новый массив evenNumbers первые три четных
+// числа из оригинального массива.
+
+// Предусмотрите выход из цикла, когда в массиве evenNumbers уже есть три значения.
+
+// const numbers = [1, 2, 2, 13, 11, 7, 18, 20, 22, 14, 15]
+
+// const evenNumbers = []
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (evenNumbers.length >= 3) {
+//     break
+//   }
+
+//   if (numbers[i] % 2 === 0) {
+//     evenNumbers.push(numbers[i])
+//   }
+// }
+
+// console.log(evenNumbers)
