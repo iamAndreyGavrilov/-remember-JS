d881f7e7574482b193ee8685a2e41b10b6c01257
Andrey G <a.gavrilov@promo-bot.ru>
2022-09-23 12:21:05 +0500

    Update app.js

--- module 4/app.js
+++ module 4/app.js
@@ -88,4 +88,51 @@
 //   }
 // }
 
-// Области видимости
+// Области видимости (Scope)
+// {} - локальная область видимости
+// - глобальная область видимости
+
+// Коллбеки и функции высшего порядка
+
+// function callback(arr, instructions) {
+//   const res = []
+//   for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i]
+//     res.push(instructions(element))
+//   }
+//   return res
+// }
+
+// function square(num) {
+//   return num ** 2
+// }
+
+// const sum = callback([1, 2, 11, 4], square)
+// console.log(sum);
+
+// Практика по созданию функций высшего порядка
+// Создайте функции-колбэки isOdd и isEven.  Обе принимают число, и возвращают true, соответственно,
+// isOdd если переданное число нечетное, а isEven - когда число четное. В противном случае функции
+// возвращают false.
+
+// function filterArray(array, callback) {
+//   const newArray = []
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (callback(array[i])) newArray.push(array[i])
+//   }
+//   return newArray
+// }
+// const numbers = [1, 2, 3, 4, 5]
+
+// function isOdd(num) {
+//   return num % 2 !== 0
+// }
+// function isEven(num) {
+//   return num % 2 === 0
+// }
+
+// // Для проверки используйте логи (раскомментируйте их после написания функций)
+// console.log(filterArray(numbers, isEven)) // Должен вывести: [2, 4]
+// console.log(filterArray(numbers, isOdd)) // Должен вывести: [1, 3, 5]
+
+// Рекурсия
