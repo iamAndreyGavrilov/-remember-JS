b9f4194829637d0bce63c7a930f0f0346a330a44
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-01 10:20:51 +0500

    Update app.js

--- module 5/app.js
+++ module 5/app.js
@@ -374,3 +374,51 @@
 //   return result
 // }
 
+// Деструктуризация массивов
+
+// Практика на деструктуризацию массивов
+// Дана функция getInfo, возвращающая массив в результате вызова. Создайте переменные carName и
+// carSeries и при помощи деструктуризации назначьте ей значения, возвращаемые функцией.
+
+// function getInfo() {
+//   return ['BMW', 'X3']
+// }
+
+// const [carName, carSeries] = getInfo()
+// console.log(carName)
+// console.log(carSeries)
+
+// Деструктуризация объектов
+
+// Практика по деструктуризации объекта
+
+// Дан объект link. Используя деструктуризацию объекта, создайте новые переменные со значениями
+// 'blank', 'link', 'anchor', 'Click me'. Имена переменных используйте согласно ключам
+// (где необходимо, используйте переименование, создав переменную с именем renamed).
+
+const link = {
+  href: '#',
+  title: 'simple link',
+  target: 'blank',
+  className: 'link',
+  id: null,
+  children: {
+    span: {
+      content: 'Click me',
+      className: 'anchor',
+    },
+  },
+}
+
+const {
+  target,
+  className,
+  children: {
+    span: { content, className: renamed },
+  },
+} = link
+
+console.log(target)
+console.log(className)
+console.log(content)
+console.log(renamed)
