0fc6dfcf548b411cf77a34dbe75cb9a7525cd342
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-02 16:20:56 +0500

    Update app.js

--- module 6/click ( accordion )/app.js
+++ module 6/click ( accordion )/app.js
@@ -11,16 +11,6 @@
 
 // Примечание 2: используйте селекторы соседей относительно доступных элементов.
 
-// const buttons = document.getElementsByClassName('accordion');
-
-// for (let i=0; i < buttons.length; i++) {
-//     buttons[i].addEventListener('click', () => {
-//         const panel = buttons[i].nextElementSibling;
-//         panel.classList.toggle('show');
-//     })
-// }
-
-// То же самое, чуть современнее
 const buttons = document.querySelectorAll('.accordion')
 buttons.forEach((btn) =>
   btn.addEventListener('click', () => {
