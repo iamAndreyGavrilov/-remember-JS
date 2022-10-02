77af0c9241803ec3990ccd42e698f4a4f618f3bd
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-02 13:04:02 +0500

    module 6

---
+++ module 6/app.js
@@ -0,0 +1,18 @@
+// Практика на получение ссылок на HTML-элементы
+// Дана html страница с разметкой. В app.js создайте переменные со следующими элементами:
+
+// в переменную title сохраните элемент заголовка h1
+
+// в переменную btn сохраните элемент кнопки button#btn
+
+// в переменную subtitle сохраните элемент h2 из второй секции (с текстом Second section)
+
+// в переменную sections сохраните все элементы section
+
+// const title = document.querySelector('h1')
+
+// const btn = document.getElementsByTagName('button')[0]
+
+// const subtitle = document.querySelector('h2.gray')
+
+// const sections = document.getElementsByTagName('section')
