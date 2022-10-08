28b49ea57c2845fbfd3fab4b28a31333110aaa2c
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-08 19:36:49 +0500

    скролл по клику

---
+++ module 7/2 плавный скролл по клику/app.js
@@ -0,0 +1,19 @@
+const moveTo = new MoveTo();
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+  moveTo.registerTrigger(btn);
+});
+
+const link = document.querySelector("a");
+
+link.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  const target = document.querySelector(this.getAttribute("href"));
+  moveTo.move(target, {
+    duration: 3000,
+  });
+}
