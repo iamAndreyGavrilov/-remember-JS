5065e90eaedb0008d24e90e16663338b28a73c29
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-09 12:40:18 +0500

    overlay scroll

---
+++ module 7/4 продвинутый скролл/app.js
@@ -0,0 +1,33 @@
+const moveTo = new MoveTo();
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+  moveTo.registerTrigger(btn);
+});
+
+const link = document.querySelector("a");
+
+link.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  const target = document.querySelector(this.getAttribute("href"));
+  moveTo.move(target, {
+    duration: 3000,
+  });
+}
+
+// тултипы
+tippy("[data-tippy-content]", {
+  theme: "light",
+  placement: "bottom",
+});
+
+// overlay scroll
+
+OverlayScrollbars(document.querySelectorAll(".overlay"), {
+  scrollbars: {
+    autoHide: "scroll",
+  },
+});
