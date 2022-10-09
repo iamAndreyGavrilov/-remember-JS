f3d63d70b3cd537b8a044c7ed70288d68a243999
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-09 19:23:31 +0500

    синхронное получение данных с сервера

--- 8 асинхронный JS/app.js
+++ 8 асинхронный JS/app.js
@@ -18,6 +18,15 @@
 
 // console.log("2");
 
+// Асинхронное получение данных с сервера
 
-
-//
+fetch("https://jsonplaceholder.typicode.com/todsos/1")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("error response");
+    }
+  })
+  .then((json) => console.log(json))
+  .catch(console.error);
