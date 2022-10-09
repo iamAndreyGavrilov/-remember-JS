71e164e3629bbebac02705017f29f4d4ae4c0cd0
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-09 19:10:50 +0500

    Promise

---
+++ 8 асинхронный JS/app.js
@@ -0,0 +1,23 @@
+// Promise
+
+// function withPromise() {
+//   return new Promise((resolve, reject) => {
+//     resolve("Hello from Promise");
+//   });
+// }
+
+// console.log("1");
+
+// setTimeout(() => {
+//   console.log("setTimeout");
+// }, 0);
+
+// withPromise()
+//   .then(console.log)
+//   .catch((value) => console.log(value) + "!!!");
+
+// console.log("2");
+
+
+
+//
