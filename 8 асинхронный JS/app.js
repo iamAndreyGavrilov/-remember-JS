ea64c0344d15535afa589172734f5afbba20482e
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-09 22:17:04 +0500

    Методы промисов all, race

--- 8 асинхронный JS/app.js
+++ 8 асинхронный JS/app.js
@@ -82,19 +82,51 @@ getUsers();
 
 // Методы промисов resolve, reject на стадии разработки
 
-async function getPictures() {
-  // const response = await fetch("https://jsonplaceholder.typicode.com/pictures");
-  // const data = await response.json();
-  // return data;
+// async function getPictures() {
+//   // const response = await fetch("https://jsonplaceholder.typicode.com/pictures");
+//   // const data = await response.json();
+//   // return data;
 
-  // сделаем заглушку, имитация вызова API https://jsonplaceholder.typicode.com/pictures,
-  //  пока бэкэнд делает апи.
+//   // сделаем заглушку, имитация вызова API https://jsonplaceholder.typicode.com/pictures,
+//   //  пока бэкэнд делает апи.
 
-  // return Promise.resolve([{ id: 1, src: "" }]);
+//   // return Promise.resolve([{ id: 1, src: "" }]);
 
-  // сделаем заглушку, имитация вызова ошибки
+//   // сделаем заглушку, имитация вызова ошибки
 
-  return Promise.reject("invalid user");
+//   // return Promise.reject("invalid user");
+// }
+
+// getPictures().then().catch();
+
+// Методы промисов all, race
+
+async function getPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  return data;
 }
 
-getPictures().then().catch();
+async function getUsers() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  return data;
+}
+
+async function getComments() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const data = await response.json();
+  return data;
+}
+
+// Promise.all([getPosts(), getUsers(), getComments()])
+//   .then((values) => {
+//     const [posts, users, comments] = values;
+//     console.log(posts, users, comments);
+//   })
+//   .catch(console.error);
+
+// Promise.race - работает как и all, только возвращает ОДИН самый быстрый результат.
+Promise.race([getPosts(), getUsers(), getComments()])
+  .then(console.log)
+  .catch(console.error);
