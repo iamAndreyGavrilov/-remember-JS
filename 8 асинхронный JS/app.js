686645f9f03f62c6e929f9fd8592b7ac91bbc387
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-09 21:50:03 +0500

    Методы промисов resolve, reject на стадии разработки

--- 8 асинхронный JS/app.js
+++ 8 асинхронный JS/app.js
@@ -60,22 +60,41 @@
 
 // Асинхронные функции
 
-async function getUsers() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await response.json();
+// async function getUsers() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await response.json();
 
-    console.log(data);
+//     console.log(data);
 
-    //   return data;
-    // возвращается Promise, значит нужен .then()
-  } catch (error) {
-    console.error(error.message);
-  }
-}
+//     //   return data;
+//     // возвращается Promise, значит нужен .then()
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// }
 
 // getUsers().then(console.log);
 
 getUsers();
 
 // const getUsers = async () => {};
+
+// Методы промисов resolve, reject на стадии разработки
+
+async function getPictures() {
+  // const response = await fetch("https://jsonplaceholder.typicode.com/pictures");
+  // const data = await response.json();
+  // return data;
+
+  // сделаем заглушку, имитация вызова API https://jsonplaceholder.typicode.com/pictures,
+  //  пока бэкэнд делает апи.
+
+  // return Promise.resolve([{ id: 1, src: "" }]);
+
+  // сделаем заглушку, имитация вызова ошибки
+
+  return Promise.reject("invalid user");
+}
+
+getPictures().then().catch();
