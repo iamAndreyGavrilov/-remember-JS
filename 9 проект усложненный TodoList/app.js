dcf8caaa6c016ad6be82187089b34b6ab8a8cca6
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-10 12:05:45 +0500

    получение данных с сервера и отрисовка полученных данных в HTML

---
+++ 9 проект усложненный TodoList/app.js
@@ -0,0 +1,61 @@
+// globals
+const todoList = document.getElementById("todo-list");
+let todos = [];
+let users = [];
+
+// attach events
+document.addEventListener("DOMContentLoaded", initApp);
+
+//basic logic
+function getUserName(userId) {
+  const user = users.find((u) => u.id === userId);
+  return user.name;
+}
+
+function printTodo({ id, userId, title, completed }) {
+  const li = document.createElement("li");
+  li.className = "todo-item";
+  li.dataset.id = id;
+  li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(
+    userId
+  )}</b> </span>`;
+
+  const status = document.createElement("input");
+  status.type = "checkbox";
+  status.checked = completed;
+
+  const close = document.createElement("span");
+  close.innerHTML = "&times;";
+  close.className = "close";
+
+  li.prepend(status);
+  li.append(close);
+
+  todoList.prepend(li);
+}
+
+//event logic
+function initApp() {
+  Promise.all([getAllTodos(), getAllUsers()])
+    .then((values) => {
+      [todos, users] = values;
+      // данные получены, далее их нужно отправить в разметку
+      todos.forEach((todo) => {
+        printTodo(todo);
+      });
+    })
+    .catch(console.error);
+}
+
+// async logic
+async function getAllTodos() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await response.json();
+  return data;
+}
+
+async function getAllUsers() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  return data;
+}
