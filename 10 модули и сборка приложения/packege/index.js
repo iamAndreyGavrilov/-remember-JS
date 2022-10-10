7d11d9a7f7490104a9864284bafe68a17cd037c1
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-10 19:56:19 +0500

    нюансы экспорта и импорта

--- 10 модули и сборка приложения/packege/index.js
+++ 10 модули и сборка приложения/packege/index.js
@@ -1,6 +1,9 @@
 import { addOne as add1, sum } from "./first";
 import { firstName } from "./second";
 
+// импорт экспорта по умолчанию и стандартный импорт
+import print, {x} from "./third";
+
 console.log("hello", firstName);
 console.log(sum(1, 5));
 console.log(add1(5));
---
+++ 10 модули и сборка приложения/packege/third.js
@@ -0,0 +1,5 @@
+export const x = 1
+
+export default function print() {
+  console.log("hello from third");
+}
