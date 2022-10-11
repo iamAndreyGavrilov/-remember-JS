2609bdc605a57548e06d6f40cef2b80ed3091048
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-11 12:28:23 +0500

    кроссбраузерность Babel

--- 10 модули и сборка приложения/packege/index.js
+++ 10 модули и сборка приложения/packege/index.js
@@ -2,7 +2,7 @@ import { addOne as add1, sum } from "./first";
 import { firstName } from "./second";
 
 // импорт экспорта по умолчанию и стандартный импорт
-import print, {x} from "./third";
+// import print, {n} from "./third";
 
 console.log("hello", firstName);
 console.log(sum(1, 5));
--- 10 модули и сборка приложения/packege/third.js
+++ 10 модули и сборка приложения/packege/third.js
@@ -1,4 +1,4 @@
-export const x = 1
+export const n = 1
 
 export default function print() {
   console.log("hello from third");
