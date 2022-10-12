a838665ebefcc8a010b6a87fa131543683126165
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-12 18:29:27 +0500

    Использование статических методов, геттеров и сеттеров

--- 12 ООП в JS/app.js
+++ 12 ООП в JS/app.js
@@ -100,51 +100,172 @@
 
 // статические свойства и методы классов
 
-class Player {
-  static totalPlayers = 0;
-  constructor(login, score = 100) {
-    this.login = login;
-    this.score = score;
+// class Player {
+//   static totalPlayers = 0;
+//   constructor(login, score = 100) {
+//     this.login = login;
+//     this.score = score;
 
-    Player.totalPlayers++;
-  }
-  increaseScore(num = 10) {
-    this.score += num;
-  }
-  decreaseScore(num = 10) {
-    this.score -= num;
+//     Player.totalPlayers++;
+//   }
+//   increaseScore(num = 10) {
+//     this.score += num;
+//   }
+//   decreaseScore(num = 10) {
+//     this.score -= num;
+//   }
+
+//   static create(login) {
+//     return new Player(login, 250);
+//   }
+
+//   static sortedByScore(a, b) {
+//     return a.score - b.score;
+//   }
+// }
+
+// class PaidPlayer extends Player {
+//   static totalPlayers = 0;
+
+//   constructor(login, score = 100, accBalance = 1000) {
+//     // в базовом варианте super вызывает родительский constructor у Player
+//     super(login, score);
+//     this.accBalance = accBalance;
+//     PaidPlayer.totalPlayers++;
+//   }
+
+//   increaseBalance(amount = 0) {
+//     this.accBalance += amount;
+//   }
+
+//   static create(login) {
+//     return new PaidPlayer(login, 250, 1000);
+//   }
+// }
+
+// const player3 = new Player("Tor");
+// const player4 = new Player("Loki", 200);
+// const pp1 = new PaidPlayer("stark", 200, 100000);
+
+// console.log(pp1);
+
+// геттеры и сеттеры
+// class Player {
+//   static totalPlayers = 0;
+//   #score;
+
+//   constructor(login, firstName, lastName, score = 100) {
+//     this.login = login;
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.#score = score;
+
+//     Player.totalPlayers++;
+//   }
+
+// вычисляемое выражение fullName
+//   get fullName() {
+//     return this.firstName + " " + this.lastName;
+//   }
+//   set fullName(name) {
+//     const [f, l] = name.split(" ");
+//     this.firstName = f;
+//     this.lastName = l;
+//   }
+
+//   get score() {
+//     return this.#score;
+//   }
+
+//   increaseScore(num = 10) {
+//     this.#score += num;
+//   }
+//   decreaseScore(num = 10) {
+//     this.#score -= num;
+//   }
+
+//   static create(login) {
+//     return new Player(login, 250);
+//   }
+
+//   static sortedByScore(a, b) {
+//     return a.#score - b.#score;
+//   }
+// }
+
+// class PaidPlayer extends Player {
+//   static totalPlayers = 0;
+
+//   constructor(login, score = 100, accBalance = 1000) {
+//     // в базовом варианте super вызывает родительский constructor у Player
+//     super(login, score);
+//     this.accBalance = accBalance;
+//     PaidPlayer.totalPlayers++;
+//   }
+
+//   increaseBalance(amount = 0) {
+//     this.accBalance += amount;
+//   }
+
+//   static create(login) {
+//     return new PaidPlayer(login, 250, 1000);
+//   }
+// }
+
+// const player3 = new Player("Tor");
+// const player4 = new Player("Loki", 200);
+// const pp1 = new PaidPlayer("stark", 200, 100000);
+
+// const p1 = new Player("m1", "vasily", "popkov", 200);
+
+// // console.log(p1.fullName);
+
+// // p1.lastName = "googlov";
+
+// // console.log(p1.fullName);
+
+// // console.log(player3.#score);
+
+// Использование статических методов, геттеров и сеттеров
+
+
+// Создайте класс Temperature, который:
+
+// в конструкторе принимает число в градусах Цельсия
+
+// может предоставлять информацию о градусах как в Цельсиях (ключ celsius), так и в Фаренгейтах
+// (ключ fahrenheit)
+
+// для экземпляра класса позволяет изменить информацию класса, как по ключу celsius, так и fahrenheit
+
+// содержит статический метод fromFahrenheit для создания экземпляра с автоматически пересчитанным
+// значением в Цельсиях (прим.: для округления значений используйте Math.round).
+
+class Temperature {
+  constructor(celsius) {
+    this.celsius = celsius;
   }
 
-  static create(login) {
-    return new Player(login, 250);
+  get fahrenheit() {
+    return this.celsius * 1.8 + 32;
   }
 
-  static sortedByScore(a, b) {
-    return a.score - b.score;
+  set fahrenheit(value) {
+    this.celsius = (value - 32) / 1.8;
+  }
+
+  static fromFahrenheit(value) {
+    return new Temperature(Math.round((value - 32) / 1.8));
   }
 }
 
-class PaidPlayer extends Player {
-  static totalPlayers = 0;
+const day1 = new Temperature(25);
+console.log(day1.celsius);
+console.log(day1.fahrenheit);
 
-  constructor(login, score = 100, accBalance = 1000) {
-    // в базовом варианте super вызывает родительский constructor у Player
-    super(login, score);
-    this.accBalance = accBalance;
-    PaidPlayer.totalPlayers++;
-  }
+day1.celsius = 26;
+day1.fahrenheit = 67;
 
-  increaseBalance(amount = 0) {
-    this.accBalance += amount;
-  }
+const day2 = new Temperature(24);
 
-  static create(login) {
-    return new PaidPlayer(login, 250, 1000);
-  }
-}
-
-const player3 = new Player("Tor");
-const player4 = new Player("Loki", 200);
-const pp1 = new PaidPlayer("stark", 200, 100000);
-
-console.log(pp1);
+const day3 = Temperature.fromFahrenheit(88);
