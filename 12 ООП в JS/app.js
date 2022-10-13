8f8ef3e6822144f232bf4d5e30274b1789af974e
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-13 16:36:15 +0500

    5) Наблюдатель

--- 12 ООП в JS/app.js
+++ 12 ООП в JS/app.js
@@ -499,5 +499,74 @@
 // const ord = new Order([{ id: 1, title: "bear", price: 200, quantity: 1 }]);
 // console.log(ord);
 
-
 //======================================================================
+
+// 5) Наблюдатель
+// Также известен как: Издатель-Подписчик, Слушатель, Observer
+
+class Post {
+  constructor(title, subtitle) {
+    this.title = title;
+    this.subtitle = subtitle;
+  }
+}
+
+class Editor {
+  #observers = [];
+
+  constructor(login, role = "editor") {
+    this.login = login;
+    this.role = role;
+    this.posts = [];
+  }
+
+  createPost(title, subtitle) {
+    const post = new Post(title, subtitle);
+    this.posts.push(post);
+    this.notify(post);
+  }
+
+  attach(observer) {
+    const isExist = this.#observers.includes(observer);
+
+    if (isExist) return;
+
+    this.#observers.push(observer);
+    console.log("editor получил observer");
+  }
+  detach(observer) {
+    const observerIndex = this.#observers.indexOf(observer);
+
+    if (observerIndex === -1) {
+      return console.log("observer не найден");
+    }
+    // находим по индексу и удаляем 1 элемент
+    this.#observers.splice(observerIndex, 1);
+    console.log("editor удалил observer");
+  }
+
+  notify(post) {
+    for (const observer of this.#observers) {
+      observer.update(this.login + " опубликовал пост" + JSON.stringify(post));
+    }
+  }
+}
+
+class Admin {
+  constructor(login, role = "admin") {
+    this.login = login;
+    this.role = role;
+  }
+  update(subject) {
+    console.log(subject);
+  }
+}
+
+const editor1 = new Editor("tor");
+const editor2 = new Editor("loki");
+const admin = new Admin("odin");
+
+editor1.attach(admin);
+editor2.attach(admin);
+
+editor1.createPost("my title", "my subtitle");
